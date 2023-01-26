eeb04ad4aea2442d59c8de1d9ad7b5d961789f41
Parth Parmar <1604pparth@gmail.com>
2023-01-26 22:02:49 +0530

    security issues fixed

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3po5dpru2i(4sp$b8$6)5cl+myh*2rabqa@-6%!0i91-s(i--p'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
